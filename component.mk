3dd1d30d9091b9cc8d786be16d6539a034dbc719
Slavey Karadzhov <slav@attachix.com>
2021-10-15 17:13:16 +0200

    Only Host and ESP32 are supported.

--- component.mk
+++ component.mk
@@ -1,10 +1,16 @@
 ## Local build configuration
 ## Parameters configured here will override default and ENV values.
-## Uncomment and change examples:
 
 export COMPONENT_SEARCH_DIRS := $(CURDIR)/Libraries
 
 COMPONENT_DEPENDS = Graphics AXP202X
 
+
+SMING_ARCH := Esp32
+
 COMPONENT_SRCDIRS := app src
 COMPONENT_INCDIRS := $(COMPONENT_SRCDIRS)  
+
+ifeq (,$(filter $(SMING_ARCH),Esp32 Host))
+$(error "Unsupported platform. Only Esp32 and Host are supported")
+endif
